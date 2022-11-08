11ae4e740736c6db802319065bf3f5c027518ff3
DanielDelgadoRebollo <72132961+DanielDelgadoRebollo@users.noreply.github.com>
2022-11-08 09:25:16 +0100

    Up

--- Despliegue de aplicaciones/login.php
+++ Despliegue de aplicaciones/login.php
@@ -4,17 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css">
-    <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
-    <title>Captcha</title>
+    <title>Document</title>
 </head>
 <body style="background-image: url(https://i.pinimg.com/736x/45/3d/74/453d7420dfeb2ca3ff0e41f33358b7e9.jpg)">
-<nav class="navbar navbar-dark bg-dark">
-<div class="container-fluid">
+<div>
+ <nav class="navbar navbar-dark bg-dark">
+ <div class="container-fluid">
     <a class="navbar-brand" href="inicio.php">App MongoDB+ PHP + bootstrap</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -22,18 +21,26 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page">Home</a>
+          <a class="nav-link active" aria-current="page" href="login.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Registro</a>
         </li>
     </div>
   </div>
-</nav>
-    <div class="container w-4/12 mx-auto">
-        <p>Utiliza el captcha para poder seguir.</p>
-    <div class="h-captcha" data-sitekey="520d0bcf-d7df-409f-adea-f51f50122351"></div>
-    <button type="submit" mat-raised-button [href]="inicio.php">Pulse aqui para seguir</button>
-    </div>  
+ </nav>
+ <div class="row">
+  <div class="col"><input type="text" class="form-control" placeholder="First name" aria-label="First name"></div>
+  <div class="col"><input type="text" class="form-control" placeholder="Last name" aria-label="Last name"></div>
+</div>
+ <?php
+
+
+
+
+
+
+ ?>
+</div>
 </body>
 </html>
