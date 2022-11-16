173491cf62545851cd29da94deb1926e4f538c05
DanielDelgadoRebollo <72132961+DanielDelgadoRebollo@users.noreply.github.com>
2022-11-16 17:24:08 +0100

    Up

---
+++ Despliegue de aplicaciones/crud/actualizar.php
@@ -0,0 +1,24 @@
+<?php
+require '../template/header.php' 
+?>
+<div style="width: 600px; height: 600px; margin:auto">
+<h1 style="text-align:center">Actualizar Registro</h1>
+<form action="" method="post">
+    <label for="nombre">Nombre</label>
+    <input type="text" class="form-control" id="nombre" name="nombre">
+    <label for="apellido">Apellido</label>
+    <input type="text" class="form-control" id="apellido" name="apellido">
+    <label for="telefono">Telefono</label>
+    <input type="text" class="form-control" id="telefono" name="telefono">
+    <label for="Mail">Mail</label>
+    <input type="text" class="form-control" id="Mail" name="Mail">
+    <label for="Direccion">Direccion</label>
+    <input type="text" class="form-control" id="Direccion" name="Direccion">
+    <label for="Localidad">Localidad</label>
+    <input type="text" class="form-control" id="Localidad" name="Localidad">
+    <a href="agregar.php" class="btn btn-warning" style="margin-top:10px">Actualizar Usuario</a>
+    
+</form>
+</div>
+</body>
+</html>
--- Despliegue de aplicaciones/welcome.php
+++
@@ -1,2 +0,0 @@
-Welcome <?php echo $_POST["user"];?><br>
-Your email adress is: <?php echo $_POST["pass"];?>
