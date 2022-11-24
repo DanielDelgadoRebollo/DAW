40c40ec4a83d20868a6a135202ff3a7ca354dce7
DanielDelgadoRebollo <72132961+DanielDelgadoRebollo@users.noreply.github.com>
2022-11-24 19:35:54 +0100

    Up

---
+++ Desarrollo entorno cliente/Tema01/Practica4/ejercicio1.js
@@ -0,0 +1,30 @@
+class Equipo{
+ nombre;
+ plantilla;
+ presupuesto;
+
+ constructor(nombre,plantilla,presupuesto){
+    this.nombre = nombre;
+    this.plantilla = plantilla;
+    this.presupuesto = presupuesto;
+ }
+
+ficharJugador(){
+
+
+}
+}
+
+class Jugador{
+nombre;
+posicion;
+valor;
+
+constructor(nombre,posicion,valor){
+this.nombre = nombre;
+this.posicion = posicion;
+this.valor = valor;
+}
+}
+
+
