7b22a16233ba347220d7b37a68180219b77a9dbb
DanielDelgadoRebollo <72132961+DanielDelgadoRebollo@users.noreply.github.com>
2022-11-26 16:46:22 +0100

    Up

--- Desarrollo entorno cliente/Tema01/Practica4/ejercicio1.js
+++ Desarrollo entorno cliente/Tema01/Practica4/ejercicio1.js
@@ -1,18 +1,35 @@
 class Equipo{
  nombre;
- plantilla;
+ plantilla = Array()
  presupuesto;
+
+ ficharJugador(jugador){
+   Array.from(this.plantilla).unshift(jugador)
+ }
+
+
 
  constructor(nombre,plantilla,presupuesto){
     this.nombre = nombre;
     this.plantilla = plantilla;
     this.presupuesto = presupuesto;
  }
+ get getNombre(){
+   return this.nombre
+ }
+ get getPlantilla(){
+   return this.plantilla
+ }
+ get getPresupuesto(){
+   return this.presupuesto
+ }
 
-ficharJugador(){
-
-
-}
+ set setNombre(nombre){
+   this.nombre = nombre;
+ }
+ mostrarDatos(){
+   console.log(`El jugador fichado es: ${this.plantilla}`);
+ }
 }
 
 class Jugador{
@@ -26,5 +43,20 @@ this.posicion = posicion;
 this.valor = valor;
 }
 }
+
+
+var nombre = 'Real Madrid';
+var plantilla = '';
+var presupuesto = 100000
+var team = new Equipo(nombre,plantilla,presupuesto);
+
+var namePlayer = 'El bicho';
+var PositionPlayer = 'Left winger';
+var valorPlayer = 100000000;
+var player = new Jugador(namePlayer,PositionPlayer,valorPlayer);
+
+team.ficharJugador('amai');
+
+
 
 
