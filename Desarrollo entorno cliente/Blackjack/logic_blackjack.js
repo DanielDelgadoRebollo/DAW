7bbcb8679e95f92b4cb6a345e245b4d2dfb9e838
DanielDelgadoRebollo <72132961+DanielDelgadoRebollo@users.noreply.github.com>
2022-11-27 20:36:38 +0100

    Up

--- Desarrollo entorno cliente/Blackjack/logic_blackjack.js
+++ Desarrollo entorno cliente/Blackjack/logic_blackjack.js
@@ -1,9 +1,50 @@
-class Carta{
-
-
-
-
+const carta = {
+palos: ['C','D','P','R'],
+numero : ['1','2','3','4','5','6','7','8','9','10','11','12','13'],
+valor : ''
 }
+
+var baraja = [];
+
+function formarBaraja() {
+    
+    for (let i = 0; i < carta.numero.length; i++) {
+        if(i<=10){
+            carta.palos.forEach(element => {
+                baraja.unshift(element + i);
+            });
+        }
+      switch(i){
+        case 11: carta.palos.forEach(element =>{
+            baraja.unshift('J'+element);
+        }); 
+        break;
+        case 12: 
+        carta.palos.forEach(element =>{
+            baraja.unshift('Q'+element);
+        }); 
+        break;
+        case 13: carta.palos.forEach(element =>{
+            baraja.unshift('K'+element);
+        }); 
+        break;
+      }
+       
+    }
+}
+
+function pedirCarta(){
+    
+}
+
+
+
+var random = Math.round(Math.random()*56);
+var manoJugador = [];
+var respuesta;
+
+
+
 
     do{
         console.log('Bienvenido al blackjack');
@@ -15,6 +56,16 @@ class Carta{
         switch(num)
         {
             case 1:console.log('Has entrado en el blackjack')
+            formarBaraja();
+            manoJugador.unshift(baraja[random])
+            console.log('Tu primera carta es ' + baraja[random])
+            respuesta = confirm('Deseas otra carta?')
+            if(confirm){
+                pedirCarta();
+            }else{
+                
+            }
+
                 break;
             case 2:console.log(reglas)
                 break;
