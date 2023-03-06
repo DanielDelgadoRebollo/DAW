47cbc92aac4d6333faaebd8a63febab1c48feb86
DanielDelgadoRebollo <72132961+DanielDelgadoRebollo@users.noreply.github.com>
2023-03-06 18:29:05 +0100

    up

--- Desarrollo entorno cliente/Tema02/01.js
+++ Desarrollo entorno cliente/Tema02/01.js
@@ -1,26 +1,39 @@
-/*fetch('https://run.mocky.io/v3/16f71bfa-7bee-476f-ab6b-71348547a9d3').then((res) =>{
-   return res.json();
-})
-.then((res) => {
-console.log(res)
-})
-.catch((rej)=>{
-    console.log(`Ha habido algun tipo de fallo: ${rej}`);
-});
-*/
+let desplegable = document.querySelector("#select_categorias");
+let textAreaCrypto = document.querySelector('#floatingTextarea');
 
-async function obtenerCryptos(){
+desplegable.addEventListener("change", function(){
+    getCryptoData(desplegable.value);
+})
+
+
+async function getNameCryptos(){
     let resultado = await fetch('https://run.mocky.io/v3/16f71bfa-7bee-476f-ab6b-71348547a9d3');
     let resultadoJSON = await resultado.json();
     return resultadoJSON;
 }
 
-obtenerCryptos().then((res)=>{
+async function getCryptoData(){
+    let resultado = await fetch('https://api.coingecko.com/api/v3/coins/0x' );
+    let resultadoJSON = await resultado.json();
+    return resultadoJSON;
+}
+
+
+getNameCryptos().then((res)=>{
       res.forEach(element => {
-        document.getElementById("select_categorias").innerHTML += `<option value="${element.id}">${element.id}</option>`;
+      desplegable.innerHTML += `<option value="${element.id}">${element.id}</option>`;
       });
 })
 .catch((err)=>{
     console.log("Error al pasar el json");
-})
+});6
 
+getCryptoData().then((res)=>{
+    res.forEach(element =>{
+    textAreaCrypto.innerHTML = '';
+    textAreaCrypto.innerHTML += `${element.description}`;
+    console.log(element.description);
+    });
+}).catch((err)=>{
+    console.log("Error al pasar el json bro");
+});
