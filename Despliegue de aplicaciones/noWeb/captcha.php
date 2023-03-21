5f4ca06af0530b0ccf8cee9fa6c9dd1ede34fd48
DanielDelgadoRebollo <72132961+DanielDelgadoRebollo@users.noreply.github.com>
2023-03-21 10:27:24 +0100

    Push

---
+++ Despliegue de aplicaciones/noWeb/captcha.php
@@ -0,0 +1,12 @@
+<?php
+require './template/header.php'
+?>
+    <div style="margin: left 400px;">
+    <div class="container w-4/12 mx-auto">
+        <p>Utiliza el captcha para poder seguir.</p>
+    <div class="h-captcha" data-sitekey="520d0bcf-d7df-409f-adea-f51f50122351"></div>
+    <button type="submit" mat-raised-button [href]="login.php">Pulse aqui para seguir</button> 
+    </div>
+</div>
+</body>
+</html>
---
+++ Despliegue de aplicaciones/noWeb/inicio.php
@@ -0,0 +1,10 @@
+<?php
+require './template/header.php'
+?>
+ <div style="text-align:center">
+  <h1>Bienvenidos a la aplicación realizada con MongoDB y PHP</h1>
+  <p style="color:#33B8FF">En esta app podremos listar, agregar, modificar y eliminar Datos</p>
+ </div>
+</div>
+</body>
+</html>
